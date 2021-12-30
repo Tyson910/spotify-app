765845ac72d3bd67c8112fd6922b062697f7f844
Tyson910 <Tyson910@lve.missouristate.edu>
2021-12-29 18:09:55 -0600

    create getAccessToken function

---
+++ src/lib/Spotify.js
@@ -0,0 +1,17 @@
+const basic = Buffer.from(`${client_id}:${client_secret}`).toString('base64');
+const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
+async function getAccessToken(refresh_token) {
+  const response = await fetch(TOKEN_ENDPOINT, {
+    method: 'POST',
+    headers: {
+      Authorization: `Basic ${basic}`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: new URLSearchParams({
+      grant_type: 'refresh_token',
+      refresh_token,
+    }),
+  });
+
+  return response.json();
+}
