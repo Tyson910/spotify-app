cbfeb98d2d8eb95d85fa12c4dc169439d48e14dc
Tyson910 <Tyson910@lve.missouristate.edu>
2021-12-29 21:28:20 -0600

    Add spotify cdn/domains to next config

--- next.config.js
+++ next.config.js
@@ -1,21 +1,22 @@
 module.exports = {
   reactStrictMode: true,
-
-  webpack: (
-    config,
-    { dev, isServer }
-  ) => {
+  images: {
+    domains: [
+      'i.scdn.co',
+      'blend-playlist-covers.spotifycdn.com',
+      'seed-mix-image.spotifycdn.com',
+      'daily-mix.scdn.co',
+      'mosaic.scdn.co'
+    ],
+  },
+  webpack: (config, { dev, isServer }) => {
     // Replace React with Preact only in client production build
     if (!dev && !isServer) {
-      Object.assign(
-        config.resolve.alias,
-        {
-          react: 'preact/compat',
-          'react-dom/test-utils':
-            'preact/test-utils',
-          'react-dom': 'preact/compat',
-        }
-      );
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
     }
 
     return config;
