d28dbb3508ac8bee7c7fa724e3d2d76059c5df22
Tyson910 <Tyson910@lve.missouristate.edu>
2021-12-29 18:08:32 -0600

    Create NextAuth handler

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,43 @@
+import NextAuth from 'next-auth';
+import SpotifyProvider from 'next-auth/providers/spotify';
+
+const scope =
+  'user-read-private user-read-email user-read-recently-played user-library-read user-top-read';
+
+const options = {
+  providers: [
+    SpotifyProvider({
+      authorization: `https://accounts.spotify.com/authorize?scope=${scope.replace(
+        ' ',
+        ','
+      )}`,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+    }),
+  ],
+  debug: true,
+  secret: process.env.SECRET,
+  callbacks: {
+    redirect({ url, baseUrl }) {
+      return baseUrl;
+    },
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.refresh_token;
+      }
+      return token;
+    },
+    async session({session, token}) {
+      // Add property to session, like an access_token from a provider.
+      if (token) {
+        session?.accessToken = token?.accessToken;
+      }
+      return session
+    },
+  },
+    pages: {
+      signIn: '/auth/signin',
+    },
+};
+
+export default (req, res) => NextAuth(req, res, options);
