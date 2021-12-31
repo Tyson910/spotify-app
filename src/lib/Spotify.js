069e79de814b9c09de9e9cda540fa5f41195fa89
Tyson910 <Tyson910@lve.missouristate.edu>
2021-12-30 19:16:27 -0600

    use axios instead of fetch API in getUsersTopTracks

--- src/lib/Spotify.js
+++ src/lib/Spotify.js
@@ -1,8 +1,11 @@
+import axios from 'axios';
+
 const client_id = process.env.CLIENT_ID;
 const client_secret = process.env.CLIENT_SECRET;
 const basic = Buffer.from(`${client_id}:${client_secret}`).toString('base64');
 const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
 const SPOTIFY_API_ENDPOINT = 'https://api.spotify.com/v1';
+const SPOTIFY_AXIOS_ENDPOINT = axios.create({ baseURL: SPOTIFY_API_ENDPOINT });
 
 async function getAccessToken(refresh_token) {
   const response = await fetch(TOKEN_ENDPOINT, {
@@ -19,7 +22,6 @@ async function getAccessToken(refresh_token) {
 
   return response.json();
 }
-
 export async function getUsersPlaylists(refresh_token) {
   const { access_token } = await getAccessToken(refresh_token);
   return fetch(`${SPOTIFY_API_ENDPOINT}/me/playlists`, {
@@ -28,12 +30,24 @@ export async function getUsersPlaylists(refresh_token) {
     },
   });
 }
-
 export async function getUsersTopTracks(refresh_token) {
   const { access_token } = await getAccessToken(refresh_token);
-  const x = await fetch(
-    `${SPOTIFY_API_ENDPOINT}/me/top/tracks`,
-    {
+  try {
+    const tracks = await SPOTIFY_AXIOS_ENDPOINT.get('/me/top/tracks', {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+      params: {
+        limit: 50,
+      },
+    });
+    const { data } = tracks;
+    return data;
+  } catch (e) {
+    console.log(e);
+    return e;
+  }
+}
       headers: {
         Authorization: `Bearer ${access_token}`,
       },
