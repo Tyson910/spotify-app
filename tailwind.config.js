ef2c25078103be7cd658f2e344324ffcb37ca64c
Tyson910 <Tyson910@lve.missouristate.edu>
2021-12-30 19:06:17 -0600

    add jit emgine to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 module.exports = {
+  mode: 'jit',
+  darkMode: 'media', // or 'media' or 'class'
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
