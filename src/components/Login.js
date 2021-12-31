7fa08a135938e804c168ce480a2a87be4e7cd061
Tyson910 <Tyson910@lve.missouristate.edu>
2021-12-30 19:13:27 -0600

    send user directly to spotify sign in page on login button click

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,14 +6,14 @@ export function Login() {
     return (
       <>
         Signed in as {session?.user?.name}, email: {session?.user?.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
+        <button onClick={() => signOut()} className="spotify-btn">Sign out</button>
       </>
     )
   }
   return (
     <>
       Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
+      <button onClick={() => signIn("spotify")} className="spotify-btn">Sign in</button>
     </>
   )
 }
