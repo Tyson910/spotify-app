411bc8f90460ffb0b16b572316fd226da9587adc
Tyson910 <Tyson910@lve.missouristate.edu>
2022-01-16 20:41:57 -0500

    set category average to 3 fixed decimal points

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,10 +1,9 @@
-//valid_keys.map(item => <Component average={calcCategoroyAverage(item, valid_keys)} />)
 export function calcCategoryAverage(category, items) {
   const sum = items.reduce(
     (total, currentValue) => total + currentValue[category],
     0
   );
   //calc average (set to 1, 2, or 3 decimal points? TBD)
-  const average = (sum / items.length);
-  return average;
+  const average = (sum / items.length).toFixed(3);
+  return parseFloat(average);
 }
