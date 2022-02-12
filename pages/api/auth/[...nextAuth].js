163d45e6e09582426a84904abb6f612bd6e3d9a4
Tyson910 <45476882+Tyson910@users.noreply.github.com>
2022-02-12 15:37:15 -0600

    Delete [...nextAuth].js

--- pages/api/auth/[...nextAuth].js
+++
@@ -1,6 +0,0 @@
-    async session({ session, token }) {
-        session.accessToken = token?.accessToken;
-      if (!session?.image || !session?.user?.image) {
-        session.user.image = null;
-      }
-      return session;
