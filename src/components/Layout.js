52d9eca59db4bd986f804677972ce94cce2767f9
Tyson910 <Tyson910@lve.missouristate.edu>
2022-02-12 15:54:48 -0500

    check for session in Layout component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,12 @@
 import { NavBar } from './NavBar';
+import { useSession } from 'next-auth/react';
+import { Login } from './Login';
 
 export function Layout({ children }) {
+  const { data: session } = useSession();
+  if (!session) {
+    return <Login />;
+  }
   return (
     <>
       <NavBar />
