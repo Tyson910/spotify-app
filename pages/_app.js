a6f37d574a99093a684d5a78ad7f0916f754989b
Tyson910 <Tyson910@lve.missouristate.edu>
2022-02-12 15:39:23 -0500

    use layout component in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import '../styles/globals.css';
 import { SessionProvider } from 'next-auth/react';
-
+import { Layout } from '../src/components/Layout';
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </SessionProvider>
   );
 }
