5057b10ab8f0df8a0e51276a31d74166630f0f81
Tyson <TysonS.417@gmail.com>
2025-04-22 19:40:02 -0500

    wire up loginform and login page

---
+++ nuxt-app/app/components/LoginForm.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+const appConfig = useAppConfig();
+
+const features = [
+  'Create playlists based on specific BPM ranges',
+  'Discover new tracks that match your workout tempo',
+  'Save custom playlists directly to your Spotify account',
+] as const;
+</script>
+
+<template>
+  <UCard>
+    <div class="flex flex-col items-center mb-8">
+      <UIcon
+        name="i-lucide-music"
+        class="text-(--ui-primary) size-14"
+      />
+      <h1 class="text-3xl font-bold text-center mb-2">
+        {{ appConfig.siteName }}
+      </h1>
+      <p class="text-gray-500 text-center">
+        Create perfect playlists based on tempo
+      </p>
+    </div>
+
+    <div class="space-y-6">
+      <div class="bg-(--ui-bg-muted) p-4 rounded-lg border border-(--ui-border)">
+        <h2 class="font-medium mb-2">
+          What you'll get:
+        </h2>
+
+        <ul class="space-y-2 text-sm text-(--ui-text-toned) list-disc pl-5">
+          <li
+            v-for="feature in features"
+            :key="feature"
+          >
+            {{ feature }}
+          </li>
+        </ul>
+      </div>
+
+      <UButton
+        :label="true ? 'Connect with Spotify' : 'Loading'"
+        block
+        loading-auto
+        size="xl"
+        class="rounded-full py-3 transition-colors duration-300"
+        icon="i-simple-icons-spotify"
+      />
+      <p class="text-xs text-center text-gray-400 mt-4">
+        By continuing, you agree to allow {{ appConfig.siteName }} to access your Spotify account data in accordance with our <NuxtLink
+          href="/terms-of-service"
+          class="text-(--ui-primary) hover:text-(--ui-color-primary-800)"
+        >Terms of Service</NuxtLink> and <NuxtLink
+          href="/privacy-policy"
+          class="text-(--ui-primary) hover:text-(--ui-color-primary-800)"
+        >Privacy Policy</NuxtLink>.
+      </p>
+    </div>
+  </UCard>
+</template>
---
+++ nuxt-app/app/pages/login.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="pt-20 mx-auto max-w-xl w-full">
+    <LoginForm />
+  </div>
+</template>
