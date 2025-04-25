a5026eed9c0e213bb34a377118c31db056a1d28a
Tyson <TysonS.417@gmail.com>
2025-04-24 20:05:37 -0500

    create BPMPreview component

---
+++ nuxt-app/app/components/BPMPreview.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import drumSfx from '~~/public/sounds/909-drums.mp3';
+
+const props = defineProps<{
+  currentBpm: number;
+}>();
+
+const { play } = useSound(drumSfx, {
+  sprite: {
+    kick: [0, 350],
+    // hihat: [374, 160],
+    // snare: [666, 290],
+    // cowbell: [968, 200],
+  },
+});
+
+const interval = computed(() => (60000 / props.currentBpm));
+
+const { pause, resume, isActive } = useIntervalFn(() => {
+  // @ts-expect-error idk why they want this to be a number
+  play({ id: 'kick' });
+}, interval, { immediate: false });
+</script>
+
+<template>
+  <div>
+    <UCard>
+      <h2 class="text-xl font-bold mb-4 flex items-center">
+        <UIcon
+          name="i-lucide-volume-2"
+          class="mr-2"
+          :size="20"
+        />
+
+        BPM Preview
+      </h2>
+      <div class="text-center space-y-4">
+        <div class="text-4xl font-mono font-bold">
+          {{ props.currentBpm }} BPM
+        </div>
+        <UButton
+          block
+          class="py-4"
+          :label="isActive ? 'Stop Preview' : 'Play Tempo'"
+          :icon="isActive ? 'i-lucide-pause' : 'i-lucide-play'"
+          @click="isActive ? pause() : resume() "
+        />
+        <p class="text-xs text-(--ui-text-muted)">
+          Hear what {{ props.currentBpm }} BPM sounds like to better understand the tempo
+        </p>
+      </div>
+    </UCard>
+  </div>
+</template>
